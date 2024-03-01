0ec9c3cc0d44a806374e325c64b0dd651ededd3c
Quang Thang <thang@thangqt.com>
2024-03-01 15:52:17 +0700

    feat: Implement Vector2D class

---
+++ include/Vector.h
@@ -0,0 +1,32 @@
+#ifndef PLATFORMER_VECTOR_H
+#define PLATFORMER_VECTOR_H
+
+#include <math.h>
+
+class Vector2D {
+public:
+    Vector2D(float x, float y): m_x(x), m_y(y) {}
+
+    float getX() { return m_x; }
+    float getY() { return m_y; }
+
+    void setX(float x) { m_x = x; }
+    void setY(float y) { m_y = y;}
+
+    float length();
+
+    Vector2D operator+(const Vector2D& v2) const;
+    friend Vector2D& operator+=(Vector2D& v1, const Vector2D& v2);
+    Vector2D operator-(const Vector2D& v2) const;
+    friend Vector2D& operator-=(Vector2D& v1, const Vector2D& v2);
+    Vector2D operator* (float scalar);
+    Vector2D& operator*=(float scalar);
+    Vector2D operator/ (float scalar);
+    Vector2D& operator/=(float scalar);
+
+private:
+    float m_x;
+    float m_y;
+};
+
+#endif //PLATFORMER_VECTOR_H
---
+++ src/Vector.cpp
@@ -0,0 +1,49 @@
+#include "Vector.h"
+
+float Vector2D::length() {
+    return sqrt(m_x * m_x + m_y * m_y);
+}
+
+Vector2D Vector2D::operator+(const Vector2D &v2) const {
+    return Vector2D(m_x + v2.m_x, m_y + v2.m_y);
+}
+
+Vector2D& operator+=(Vector2D& v1, const Vector2D& v2){
+    v1.m_x += v2.m_x;
+    v1.m_y += v2.m_y;
+
+    return v1;
+}
+
+Vector2D Vector2D::operator-(const Vector2D &v2) const {
+    return Vector2D(m_x - v2.m_x, m_y - v2.m_y);
+}
+
+Vector2D& operator-=(Vector2D& v1, const Vector2D& v2){
+    v1.m_x -= v2.m_x;
+    v1.m_y -= v2.m_y;
+
+    return v1;
+}
+
+Vector2D Vector2D::operator*(float scalar) {
+    return Vector2D(m_x * scalar, m_y * scalar);
+}
+
+Vector2D& Vector2D::operator*=(float scalar) {
+    m_x *= scalar;
+    m_y *= scalar;
+
+    return *this;
+}
+
+Vector2D Vector2D::operator/(float scalar) {
+    return Vector2D(m_x / scalar, m_y / scalar);
+}
+
+Vector2D& Vector2D::operator/=(float scalar) {
+    m_x /= scalar;
+    m_y /= scalar;
+
+    return *this;
+}
