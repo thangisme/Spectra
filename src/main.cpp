e9fe15c1727afa88fb9744948a33bda4ccba1673
Quang Thang <thang@thangqt.com>
2024-03-05 13:44:45 +0700

    chore: Add a delay of 60ms to the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <SDL.h>
 #include "Game.h"
 
-
 int main(int argc, char* args[]) {
     if (!Game::Instance() -> init ("Platformer", 100, 100, 1280, 720, 0)) return 1;
-
     while (Game::Instance() -> running()) {
         Game::Instance() -> handleEvents();
         Game::Instance() -> update();
         Game::Instance() -> render();
+
+        SDL_Delay(60);
     }
 
     Game::Instance() -> clean();
