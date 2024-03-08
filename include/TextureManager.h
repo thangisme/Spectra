0bbbe538d99e01a87d5c06586e9a20e7f66d7ee8
Quang Thang <thang@thangqt.com>
2024-03-08 16:00:51 +0700

    fix: add clearFromTextureMap method to TextureManager

--- include/TextureManager.h
+++ include/TextureManager.h
@@ -19,6 +19,7 @@ public:
     bool load(std::string fileName, std::string id, SDL_Renderer* pRenderer);
     void draw(std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
     void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void clearFromTextureMap(std::string id);
 private:
     TextureManager(){};
     static TextureManager* s_pInstance;
