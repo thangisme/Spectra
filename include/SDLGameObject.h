0fbf8732787b150fad0578df030440074982a1cb
Quang Thang <thang@thangqt.com>
2024-03-08 23:28:53 +0700

    feat: add get pos, height and width method

--- include/SDLGameObject.h
+++ include/SDLGameObject.h
@@ -13,6 +13,10 @@ public:
     virtual void draw();
     virtual void update();
     virtual void clean();
+
+    Vector2D& getPosition() { return m_pos; }
+    int getWidth() { return m_width; }
+    int getHeight() { return m_height; }
 protected:
     Vector2D m_pos;
     Vector2D m_velocity;
