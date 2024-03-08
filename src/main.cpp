0280c1a981399a895a59550e0e9162a7ad17fcd7
Quang Thang <thang@thangqt.com>
2024-03-08 22:56:56 +0700

    refactor: make window properties global const

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <SDL.h>
 #include "Game.h"
 
+const char* TITLE = "Platformer";
+const int WINDOW_WIDTH = 1920;
+const int WINDOW_HEIGHT = 1080;
+const int INITIAL_X = SDL_WINDOWPOS_CENTERED;
+const int INITIAL_Y = SDL_WINDOWPOS_CENTERED;
+const int SDL_FLAG = 0;
+
 const int FPS = 60;
 const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* args[]) {
     Uint32 frameStart, frameTime;
 
-    if (!Game::Instance() -> init ("Platformer", 100, 100, 1280, 720, 0)) return 1;
+    if (!Game::Instance() -> init (TITLE, INITIAL_X, INITIAL_Y, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_FLAG)) return 1;
     while (Game::Instance() -> running()) {
         frameStart = SDL_GetTicks();
 
