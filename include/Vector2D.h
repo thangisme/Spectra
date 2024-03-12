c8883be17fb8d9af7184c4c111f9b3434c98b228
Quang Thang <thang@thangqt.com>
2024-03-12 20:59:28 +0700

    fix: Vector2D should default parameters to 0

--- include/Vector2D.h
+++ include/Vector2D.h
@@ -5,7 +5,7 @@
 
 class Vector2D {
 public:
-    Vector2D(float x, float y): m_x(x), m_y(y) {}
+    Vector2D(float x = 0.0f, float y = 0.0f): m_x(x), m_y(y) {}
 
     float getX() { return m_x; }
     float getY() { return m_y; }
