fa53a54ec151f804283602444d624139831b0305
Quang Thang <thang@thangqt.com>
2024-03-19 21:43:19 +0700

    chore: change turret health

--- src/Turret.cpp
+++ src/Turret.cpp
@@ -1,7 +1,7 @@
 #include "Turret.h"
 #include "BulletHandler.h"
 
-Turret::Turret() : m_dyingTime(1000), m_health(100), m_bulletFiringSpeed(50){}
+Turret::Turret() : m_dyingTime(1000), m_health(10), m_bulletFiringSpeed(50){}
 
 Turret::~Turret(){}
 
